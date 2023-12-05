c1ba636acd63d40edbf3a7f6cdee6536ca13c0e9
paula <boye.boyar@gmail.com>
2023-12-05 15:46:39 +0100

    got exercise 3 correct

--- script.js
+++ script.js
@@ -25,15 +25,20 @@ export function exercise02(args) {
   return result.join("")
 }
 
+
 export function exercise03(args) {
   const input = args
   const result = []
-
+  let count = 0
+ 
   for (let i = 0; i < input.length; i++) {
-
-    
+    const currentElement = input[i]
+if (currentElement === 'e'){
+  count = count + 1
+}
+else if (currentElement === 'E'){
+  count = count + 1
+}
   }
-
-
-
+  return count
 }
